3234ca5384af3dd4f6a2eda9d4129b872914a317
Prajay1610 <prajaypc16@gmail.com>
2023-08-24 19:48:56 +0530

    final ejs commit

--- EJS/server.js
+++ EJS/server.js
@@ -5,7 +5,44 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("home.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+//profile page
+app.get("/profile", (req, res) => {
+  let user = {
+    name: "Prajay",
+    age: 20,
+    city: "Nashik",
+  };
+  res.render("profile.ejs", {
+    user,
+  });
+});
+
+app.get("/posts", (req, res) => {
+  const posts = [
+    {
+      title: "HTML",
+      desc: "The best post ever HTML",
+    },
+    {
+      title: "CSS",
+      desc: "The best post ever CSS",
+    },
+    {
+      title: "JS",
+      desc: "The best post ever JS",
+    },
+  ];
+  res.render("posts.ejs", { posts });
 });
 
 app.listen(9000, function () {
